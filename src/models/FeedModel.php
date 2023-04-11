03c14a9540a892c59bc29d526af63f7ecfe9a835
Christian Røen <rqen1337@gmail.com>
2023-04-11 10:30:31 +0200

    upd

--- src/models/FeedModel.php
+++ src/models/FeedModel.php
@@ -238,13 +238,19 @@ class FeedModel extends Model
      */
     public function getNextPagination(): bool
     {
-        if (!$this->paginationUrl || !filter_var($this->paginationUrl, FILTER_VALIDATE_URL)) {
-            return false;
-        }
-
+        echo "\n";
+        echo "PaginationUrl: \n";
+        print_r($this->paginationUrl);
+        echo "\n";
+        echo "PaginationNode: \n";
+        print_r($this->paginationNode);
         if ($this->paginationURLPrefix) {
             $this->feedUrl = $this->paginationURLPrefix . $this->paginationNode;
             return true;
+        }
+
+        if (!$this->paginationUrl || !filter_var($this->paginationUrl, FILTER_VALIDATE_URL)) {
+            return false;
         }
 
         // Set the URL dynamically on the feed, then kick off processing again
